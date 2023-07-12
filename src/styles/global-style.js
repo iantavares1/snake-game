53ffe217c5de19f989b22305d8a6d6ab9a2ed8a1
Ian Tavares <iantavares2405@gmail.com>
2023-07-12 18:35:59 -0300

    init project

---
+++ src/styles/global-style.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  :root {
+    font-size: 62.5%;
+  }
+  
+  body {
+    font-size: 1.6rem;
+    font-family: "Inter", sans-serif;
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    place-content: center;
+  }
+`
